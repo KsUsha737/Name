921e73f91b1407b2edd7160fa471a62daf00264d
KsUsha737 <ok6sana@gmail.com>
2022-04-24 21:28:36 +0300

    delit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Namy");
-        System.out.println("By");
+//        System.out.println("By");
     }
 }
